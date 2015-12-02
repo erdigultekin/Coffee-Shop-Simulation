75664749f81836e0bed961d5a4131e4356429763
Erdi Gültekin <erdi.gultekin@ozu.edu.tr>
2015-12-02 12:26:06 +0200

    NEW PROJECT

---
+++ src/Server/inventoryPriceTable.java
@@ -0,0 +1,12 @@
+package Server;
+import java.io.Serializable;
+
+public class inventoryPriceTable implements Serializable{
+	
+	private static final long serialVersionUID = 7526472295622776149L;
+	
+	public double cupPrice = 5;
+	public double coffeePrice = 5;
+	public double milkPrice = 5;
+	public double sugarPrice = 5;
+}
